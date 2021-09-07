20f7a0655e61ffd0a295f75dc58973f08d9f09a9
Assassin#1234 <chordsandfrets@gmail.com>
2021-09-08 00:04:32 +0530

    feat(AIML): finished API 🎉

---
+++ main.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();const AIMLInterpreter = require('aimlinterpreter');
+const aimlInterpreter = new AIMLInterpreter({ name:'Nuggies', age:'18' });
+const AIMLFiles = fs.readdirSync('./AIML');
+aimlInterpreter.loadAIMLFilesIntoArray(AIMLFiles.map(file => `${__dirname}/AIML/${file}`));
+const port = 3000;
+
+app.set('json spaces', 1);
+app.use('/public', express.static('public'));
+app.use(bodyParser.json());
+app.get('/chat', (req, res) => {
+	const message = req.query.message;
+
+	if(!message) {
+		res.status = 400;
+		return res.send('message not provided.');
+	}
+
+	aimlInterpreter.findAnswerInLoadedAIMLFiles(message, async (answer, wildCardArray, input) => {
+
+		answer = answer != null ? answer : '';
+		res.send({
+			reply: answer,
+			wildcard: wildCardArray,
+		});
+	});
+});
+app.listen(port);
+console.log(`server starting on port ${port}`);
